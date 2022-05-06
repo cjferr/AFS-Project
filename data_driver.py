45cb0e3d948e8246ae655ecdc9259f5b2ec22e60
cjferr <cjferr@umich.edu>
2022-05-06 12:14:51 -0400

    data manually added cannot be overwritten now

--- data_driver.py
+++ data_driver.py
@@ -18,7 +18,7 @@ import random
 
 
 def append_to_cell(row, index, text):
-    if row[index] is "":
+    if row[index] == "":
         row[index] = text
     elif text == row[index]:
         pass
@@ -28,6 +28,13 @@ def append_to_cell(row, index, text):
 
 
 data = []
+
+
+def fill_cell(row, col_idx, content):
+    if not row[col_idx]:
+        row[col_idx] = content
+
+
 with open('Data_in.csv') as in_csv:
     with open('data_out.csv', 'w') as out_csv:
         writer = csv.writer(out_csv, delimiter=',', quotechar='|')
@@ -38,167 +45,168 @@ with open('Data_in.csv') as in_csv:
         for i, row in enumerate(reader):
             obj = row[0].lower()
             if "run" in obj:
-                row[2] = "lower pull"
-                row[3] = "cardio"
-                row[4] = "high impact"
-                row[5] = "posterior"
+                fill_cell(row, 2, "lower pull")
+
+                fill_cell(row, 3, "cardio")
+                fill_cell(row, 4, "high impact")
+                fill_cell(row, 5, "posterior")
                 if "step" in obj:
-                    row[6] = 2
+                    fill_cell(row, 6, 2)
                     append_to_cell(row, 7, "steps")
             if "step" in obj and "stepping" not in obj:
-                row[6] = 2
+                fill_cell(row, 6, 2)
                 append_to_cell(row, 7, "steps")
                 if "up" in obj:
-                    row[2] = "lower push"
-                    row[3] = "strength"
-                    row[4] = "low impact"
-                    row[5] = "posterior"
+                    fill_cell(row, 2, "lower push")
+                    fill_cell(row, 3, "strength")
+                    fill_cell(row, 4, "low impact")
+                    fill_cell(row, 5, "posterior")
             if "press" in obj:
-                row[2] = "upper push"
-                row[3] = "strength"
-                row[5] = "anterior"
+                fill_cell(row, 2, "upper push")
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 5, "anterior")
             if "oh" in obj:
-                row[2] = "upper push"
-                row[3] = "strength"
-                row[5] = "anterior"
+                fill_cell(row, 2, "upper push")
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 5, "anterior")
             if "crunch" in obj:
-                row[2] = "abs"
-                row[3] = "strength"
-                row[4] = "low impact"
-                row[5] = "anterior"
+                fill_cell(row, 2, "abs")
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 4, "low impact")
+                fill_cell(row, 5, "anterior")
             if "row" in obj:
-                row[2] = "upper pull"
-                row[3] = "strength"
-                row[4] = "low impact"
-                row[5] = "posterior"
+                fill_cell(row, 2, "upper pull")
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 4, "low impact")
+                fill_cell(row, 5, "posterior")
             if "curl" in obj:
-                row[2] = "upper pull"
-                row[3] = "strength"
-                row[4] = "low impact"
-                row[5] = "anterior"
+                fill_cell(row, 2, "upper pull")
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 4, "low impact")
+                fill_cell(row, 5, "anterior")
             if "barbell" in obj or "bb" in obj:
-                row[6] = 4
+                fill_cell(row, 6, 4)
                 append_to_cell(row, 7, "barbell")
-                row[3] = "strength"
-                row[4] = "low impact"
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 4, "low impact")
             if "wp" in obj:
                 append_to_cell(row, 7, "plate")
-                row[3] = "strength"
-                row[4] = "low impact"
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 4, "low impact")
             if "squat" in obj:
-                row[2] = "lower push"
+                fill_cell(row, 2, "lower push")
                 if "front" in obj:
-                    row[5] = "anterior"
+                    fill_cell(row, 5, "anterior")
                 else:
-                    row[5] = "posterior"
+                    fill_cell(row, 5, "posterior")
                 if "jump" in obj:
-                    row[3] = "cardio"
+                    fill_cell(row, 3, "cardio")
                 else:
-                    row[3] = "strength"
+                    fill_cell(row, 3, "strength")
             if "lunge" in obj or "stepping" in obj:
-                row[2] = "lower push"
+                fill_cell(row, 2, "lower push")
                 if "jump" in obj:
-                    row[3] = "cardio"
+                    fill_cell(row, 3, "cardio")
                 else:
-                    row[3] = "strength"
-                row[5] = "posterior"
+                    fill_cell(row, 3, "strength")
+                fill_cell(row, 5, "posterior")
             if "jump" in obj or "hop" in obj or "skip" in obj:
-                row[4] = "high impact"
-                row[3] = "cardio"
-                row[2] = "lower push"
+                fill_cell(row, 4, "high impact")
+                fill_cell(row, 3, "cardio")
+                fill_cell(row, 2, "lower push")
             elif i == 0:
                 pass
             else:
-                row[4] = "low impact"
+                fill_cell(row, 4, "low impact")
             if "db" in obj:
                 append_to_cell(row, 7, "db")
-                row[6] = 1
-                row[3] = "strength"
+                fill_cell(row, 6, 1)
+                fill_cell(row, 3, "strength")
             if "plank" in obj:
-                row[2] = "abs"
-                row[4] = "low impact"
-                row[5] = "anterior"
+                fill_cell(row, 2, "abs")
+                fill_cell(row, 4, "low impact")
+                fill_cell(row, 5, "anterior")
             if "boxing" in obj:
-                row[6] = 5
+                fill_cell(row, 6, 5)
                 append_to_cell(row, 7, "boxing gear")
-                row[3] = "cardio"
-                row[2] = "upper push"
-                row[5] = "anterior"
+                fill_cell(row, 3, "cardio")
+                fill_cell(row, 2, "upper push")
+                fill_cell(row, 5, "anterior")
             if "suspension" in obj or "strap" in obj:
-                row[6] = 3
+                fill_cell(row, 6, 3)
                 append_to_cell(row, 7, "suspension")
-                row[4] = "low impact"
+                fill_cell(row, 4, "low impact")
             if "press" in obj or 'push' in obj:
-                row[2] = "upper push"
-                row[3] = "strength"
-                row[5] = "anterior"
+                fill_cell(row, 2, "upper push")
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 5, "anterior")
             if "up" in obj and "down" in obj:
-                row[2] = "upper push"
-                row[3] = "cardio"
-                row[4] = "high impact"
-                row[5] = "anterior"
+                fill_cell(row, 2, "upper push")
+                fill_cell(row, 3, "cardio")
+                fill_cell(row, 4, "high impact")
+                fill_cell(row, 5, "anterior")
             if "mb" in obj:
                 append_to_cell(row, 7, "med ball")
-                row[6] = 2
+                fill_cell(row, 6, 2)
             if "band" in obj:
-                row[7] = "band"
-                row[6] = 4
+                fill_cell(row, 7, "band")
+                fill_cell(row, 6, 4)
             if "stability ball" in obj:
                 append_to_cell(row, 7, "stability ball")
-                row[6] = 1
+                fill_cell(row, 6, 1)
             if "bridge" in obj:
-                row[2] = "lower push"
-                row[3] = "strength"
-                row[4] = "low impact"
-                row[5] = "posterior"
+                fill_cell(row, 2, "lower push")
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 4, "low impact")
+                fill_cell(row, 5, "posterior")
             if "v " in obj:
-                row[2] = "abs"
-                row[3] = "strength"
-                row[5] = "anterior"
+                fill_cell(row, 2, "abs")
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 5, "anterior")
             if "hurdle" in obj:
                 append_to_cell(row, 7, "hurdle")
-                row[6] = 5
+                fill_cell(row, 6, 5)
             if "extension" in obj:
                 if "tricep" in obj or "oh" in obj:
-                    row[2] = "upper push"
+                    fill_cell(row, 2, "upper push")
                 else:
-                    row[2] = "lower push"
-                row[3] = "strength"
-                row[4] = "low impact"
-                row[5] = "posterior"
+                    fill_cell(row, 2, "lower push")
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 4, "low impact")
+                fill_cell(row, 5, "posterior")
             if "versa" in obj:
                 append_to_cell(row, 7, "versabag")
-                row[6] = 4
+                fill_cell(row, 6, 4)
             if "ladder" in obj:
-                row[6] = 4
+                fill_cell(row, 6, 4)
                 append_to_cell(row, 7, "ladder")
-                row[2] = "lower push"
-                row[3] = "cardio"
-                row[4] = "low impact"
-                row[5] = "posterior"
+                fill_cell(row, 2, "lower push")
+                fill_cell(row, 3, "cardio")
+                fill_cell(row, 4, "low impact")
+                fill_cell(row, 5, "posterior")
             if "wall sit" in obj:
-                row[2] = "lower push"
-                row[3] = "strength"
-                row[4] = "low impact"
-                row[5] = "anterior"
+                fill_cell(row, 2, "lower push")
+                fill_cell(row, 3, "strength")
+                fill_cell(row, 4, "low impact")
+                fill_cell(row, 5, "anterior")
             if "chop" in obj:
-                row[5] = "anterior"
+                fill_cell(row, 5, "anterior")
             if "step" in obj and "stepping" not in obj:
-                row[6] = 2
+                fill_cell(row, 6, 2)
                 append_to_cell(row, 7, "steps")
                 if "up" in obj:
-                    row[2] = "lower push"
-                    row[3] = "strength"
-                    row[4] = "low impact"
-                    row[5] = "posterior"
+                    fill_cell(row, 2, "lower push")
+                    fill_cell(row, 3, "strength")
+                    fill_cell(row, 4, "low impact")
+                    fill_cell(row, 5, "posterior")
             if "sandbell" in obj:
-                row[7] = "sandbell"
-                row[6] = 4
-            if row[7] is "":  # need to find a way where I can make sure things that are L and R are grouped together; maybe delete the right options and if it picks left than just tell them to make it alternating?
-                row[6] = random.choice(bw_stations)
-                row[7] = "BW"
-            if row[2] is "abs":
-                row[6] = random.choice(abs_stations)
+                fill_cell(row, 7, "sandbell")
+                fill_cell(row, 6, 4)
+            if row[7] == "":  # need to find a way where I can make sure things that are L and R are grouped together; maybe delete the right options and if it picks left than just tell them to make it alternating?
+                fill_cell(row, 6, random.choice(bw_stations))
+                fill_cell(row, 7, "BW")
+            if row[2] == "abs":
+                fill_cell(row, 6, random.choice(abs_stations))
 
             writer.writerow(row)
             data.append(row)
